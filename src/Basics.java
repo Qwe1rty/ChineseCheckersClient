5276bf76de2c9ba5403177b6d411b47e31a3bb55
rlu0 <rayluo1998@gmail.com>
2016-05-13 12:17:37 -0400

    Added a file with the opening hard code in it

---
+++ src/Basics.java
@@ -0,0 +1,73 @@
+
+class Basics {
+	int[][] array = new int[17][17];
+	int color;
+	int[] nextMove = new int[4];
+	String go = "4";
+
+	public void Opening() {
+		boolean done = false;
+		int step = 1;
+		if (color == 1) {
+			while (!done) {
+				if (!this.canMove(nextMove[0], nextMove[1]))
+					break;
+				if (go.equals("4")) {
+					if (step == 1) {
+						//y,x
+						nextMove[0] = 12;
+						nextMove[1] = 13;
+						nextMove[2] = 11;
+						nextMove[3] = 12;
+						//Send move
+
+					} else if (step == 2) {
+						nextMove[0] = 12;
+						nextMove[1] = 15;
+						nextMove[2] = 10;
+						nextMove[3] = 11;
+					} else if (step == 3) {
+						nextMove[0] = 10;
+						nextMove[1] = 14;
+						nextMove[2] = 10;
+						nextMove[3] = 10;
+					} else if (step == 4) {
+						nextMove[0] = 10;
+						nextMove[1] = 10;
+						nextMove[2] = 9;
+						nextMove[3] = 9;
+					} else if (step == 5) {
+						nextMove[0] = 12;
+						nextMove[1] = 16;
+						nextMove[2] = 8;
+						nextMove[3] = 8;
+					}
+
+					step++;
+				}
+
+			}
+		} else if (color == 2) {
+
+		}
+
+		else if (color == 3) {
+
+		} else if (color == 4) {
+
+		} else if (color == 5) {
+
+		} else if (color == 6) {
+
+		}
+
+	}
+
+	public boolean canMove(int x, int y)
+
+	{
+
+		return true;
+	}
+
+}
